9d6ee301bf9cf9debaef127892543f0227c9da1d
vallreey <revalsapertama@gmail.com>
2023-11-08 07:16:04 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-const userrouter = require('./router/user')
+const userrouter = require('./router/users')
 
 const port = 3000
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
